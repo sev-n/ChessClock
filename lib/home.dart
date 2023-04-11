922fae571d70386df67e7ec24db7c8a4529873ac
sev-n <nielsevilla39@gmail.com>
2023-04-11 22:47:38 +0800

    Added gesturedetector

--- lib/home.dart
+++ lib/home.dart
@@ -14,15 +14,31 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   //int secs = 60;
 
-  @override
-  void initState() {
+  // @override
+  // void initState() {
+  //   Timer.periodic(const Duration(seconds: 1), (t) {
+  //     var p1 = Provider.of<PlayerOne>(context, listen: false);
+  //     var p2 = Provider.of<PlayerTwo>(context, listen: false);
+  //     p1.updateTime();
+  //     p2.updateTime();
+  //   });
+  //   super.initState();
+  // }
+
+  void playerOne() {
     Timer.periodic(const Duration(seconds: 1), (t) {
       var p1 = Provider.of<PlayerOne>(context, listen: false);
-      var p2 = Provider.of<PlayerTwo>(context, listen: false);
       p1.updateTime();
-      p2.updateTime();
+      p1.toggleClicked();
     });
-    super.initState();
+  }
+
+    void playerTwo() {
+    Timer.periodic(const Duration(seconds: 1), (t) {
+      var p2 = Provider.of<PlayerTwo>(context, listen: false);
+      p2.updateTime();
+      p2.toggleClicked();
+    });
   }
 
   @override
@@ -31,39 +47,56 @@ class _HomeState extends State<Home> {
       body: SafeArea(
         child: Stack(
           children: [
-            Container(
-              alignment: Alignment.center,
-              width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.height * 0.5,
-              color: Colors.grey,
-              child: Consumer<PlayerOne>(
-                builder: (context, p1, child) {
-                  return Text(
-                    p1.getTime().toString(),
-                    style: const TextStyle(
-                      fontSize: 60,
-                    ),
-                  );
-                },
+            GestureDetector(
+              onTap: () {
+                playerOne();
+              },
+              child: Container(
+                alignment: Alignment.center,
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height * 0.45,
+                color: Colors.grey,
+                child: Consumer<PlayerOne>(
+                  builder: (context, p1, child) {
+                    return Text(
+                      p1.getTime().toString(),
+                      style: const TextStyle(
+                        fontSize: 60,
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ),
+            Center(
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height * 0.1,
+                color: Colors.black,
               ),
             ),
             Align(
               alignment: Alignment.bottomCenter,
-              child: Container(
+              child: GestureDetector(
+                onTap: (){
+                  playerTwo();
+                },
+                child: Container(
                   alignment: Alignment.center,
                   width: MediaQuery.of(context).size.width,
-                  height: MediaQuery.of(context).size.height * 0.5,
+                  height: MediaQuery.of(context).size.height * 0.45,
                   color: Colors.blueGrey,
                   child: Consumer<PlayerTwo>(
-                builder: (context, p2, child) {
-                  return Text(
-                    p2.getTime().toString(),
-                    style: const TextStyle(
-                      fontSize: 60,
-                    ),
-                  );
-                },
-              ),
+                    builder: (context, p2, child) {
+                      return Text(
+                        p2.getTime().toString(),
+                        style: const TextStyle(
+                          fontSize: 60,
+                        ),
+                      );
+                    },
+                  ),
+                ),
               ),
             )
           ],
